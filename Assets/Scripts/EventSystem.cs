b4f210f5669e1c3767ad779679c5811d4a71206d
pswxxk <sangwook1205@g.shingu.ac.kr>
2024-09-20 10:19:53 +0900

    Feat: Action, Event, Command 사용

---
+++ Assets/Scripts/EventSystem.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystem : MonoBehaviour
+{
+    public static event Action<int> OnScoreChanged;
+    public static event Action OnGameOver;
+
+    private int score = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            score += 10;
+            OnScoreChanged?.Invoke(score);
+        }
+        if (score >= 100)
+        {
+            OnGameOver?.Invoke();
+        }
+    }
+}
